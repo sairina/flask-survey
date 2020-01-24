5cb8b71a2c8c8d1a1658e11b2a6449e50294fa8f
George Gruner <imgeorge220@gmail.com>
2020-01-23 17:16:36 -0800

    added session and out-of-order url handling

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'oh-so-secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
-
-responses = []
 
 
 @app.route('/')
@@ -17,10 +16,30 @@ def index():
     return render_template('index.html', survey=survey)
 
 
+@app.route('/', methods=['POST'])
+def set_session():
+    """  """
+
+    session['responses'] = []
+    return redirect('/questions/0')
+
+
 @app.route('/questions/<int:question_num>')
 def ask_question(question_num):
     """ Shows survey question"""
+    
+    responses = session['responses']
 
+    if None in responses:
+        flash('Invalid question. Please answer the question below')
+        None_index = str(responses.index(None))
+        responses.pop()
+        return redirect('/questions/' + None_index)
+
+    elif len(responses) != question_num:
+        flash('Invalid question. Please answer the question below')
+        return redirect(f"/questions/{str(len(responses))}")
+    
     question = survey.questions[question_num]
     return render_template('question.html', question_num=question_num, question=question)
 
@@ -30,10 +49,12 @@ def send_answer():
     """ Collects survey answers in response list """
 
     answer = request.form.get("response")
+    responses = session['responses']
     responses.append(answer)
+    session['responses'] = responses
     
-    if len(responses) < len(survey.questions):
-        return redirect('/questions/' + str(len(responses)))
+    if len(session['responses']) < len(survey.questions):
+        return redirect(f"/questions/{str(len(session['responses']))}")
     else:
         return redirect('/thank_you')
 
@@ -42,5 +63,5 @@ def send_answer():
 def thank_user():
     """ Thanks users for completing survey """
 
-    print(responses)
+    print(session['responses'])
     return "Thanks"
